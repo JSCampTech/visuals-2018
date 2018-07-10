840873342734983f85cda48ef15080baaa2bbf37
Jaume Sanchez <the.spite@gmail.com>
2018-07-11 00:14:49 +0100

    added logo to intro

--- sections/intro.js
+++ sections/intro.js
@@ -6,12 +6,21 @@ class Intro extends Layer {
   constructor(renderer) {
     super(renderer);
     console.log('intro ctor');
-    this.cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1,1,1), new THREE.MeshNormalMaterial());
-    //this.scene.add(this.cube);
+
+    const loader = new THREE.TextureLoader();
+    this.plane = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(1,1),
+      new THREE.MeshBasicMaterial({
+        map: loader.load('assets/logo-mask-inverted.png'),
+        transparent: true,
+      })
+    );
+    this.plane.scale.setScalar(5);
+    this.scene.add(this.plane);
 
     this.camera = new THREE.PerspectiveCamera( 70, 1, .1, 10000 );
     this.camera.target = new THREE.Vector3( 0, 0, 0 );
-    this.camera.position.set( 2.5, 2.5, 2.5 );
+    this.camera.position.set(0, 0, 5 );
     this.camera.lookAt( this.camera.target );
     this.scene.add( this.camera );
   }
