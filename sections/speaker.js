6935e93f973165ca4c866c39387bc57b838df24d
Jaume Sanchez <the.spite@gmail.com>
2018-07-10 23:42:36 +0100

    added text shaders and more layout and animations

--- sections/speaker.js
+++ sections/speaker.js
@@ -2,6 +2,8 @@ import THREE from '../third-party/three.js';
 import Layer from '../js/layer.js';
 import speakers from '../assets/speakers.js';
 import {FontAtlas, Text} from '../js/text.js';
+import speakerVertexShader from '../shaders/speaker-vs.js';
+import speakerFragmentShader from '../shaders/speaker-fs.js';
 
 class Speaker extends Layer {
 
@@ -9,11 +11,12 @@ class Speaker extends Layer {
     super(renderer);
     console.log('Speaker ctor');
 
+    this.names = {};
+
     this.montserratAtlas = new FontAtlas( {
       renderer: renderer,
       fontName: 'Montserrat',
       fontStyle: '',
-      woffSrc: 'url(https://fonts.gstatic.com/s/shadowsintolight/v6/clhLqOv7MXn459PTh0gXYMdQSYiIg2Yb25Hg13-ek1M.woff)',
       woff2Src: `url(https://fonts.gstatic.com/s/montserrat/v12/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2) format('woff2')`
     }, () => {
       this.speakerName = new Text(this.montserratAtlas);
@@ -26,19 +29,20 @@ class Speaker extends Layer {
       renderer: renderer,
       fontName: 'Montserrat',
       fontStyle: 'bold',
-      woffSrc: 'url(https://fonts.gstatic.com/s/shadowsintolight/v6/clhLqOv7MXn459PTh0gXYMdQSYiIg2Yb25Hg13-ek1M.woff)',
       woff2Src: `url(https://fonts.gstatic.com/s/montserrat/v12/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2) format('woff2')`
     }, () => {
+      this.titleTalk = new THREE.Group();
       this.talkTitleLines = [];
       for (let j=0; j<3; j++) {
         const line = new Text(this.montserratBoldAtlas);
-        line.mesh.position.y = -.4- j * .4;
+        line.mesh.position.y = -.8 - j * .4;
         line.mesh.position.x = -1.2;
         line.mesh.scale.setScalar(4);
         line.set('');
-        this.scene.add( line.mesh );
+        this.titleTalk.add( line.mesh );
         this.talkTitleLines.push(line);
       }
+      this.scene.add(this.titleTalk);
     } );
 
     this.speakers = new Map();
@@ -46,9 +50,36 @@ class Speaker extends Layer {
 
     this.plane = new THREE.Mesh(
       new THREE.PlaneBufferGeometry(1,1),
-      new THREE.MeshBasicMaterial({}
-    ));
-   this.scene.add(this.plane);
+      new THREE.RawShaderMaterial({
+        uniforms: {
+          map: { value: null }
+        },
+        vertexShader: speakerVertexShader,
+        fragmentShader: speakerFragmentShader,
+        depthTest: false,
+        depthWrite: true,
+      })
+    );
+    this.plane.position.x = -2;
+    this.plane.position.z = -1;
+    this.plane.rotation.set(.05,.05,.05);
+    this.scene.add(this.plane);
+
+    this.nameplate = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(1,1),
+      new THREE.MeshBasicMaterial({
+        color: 0xff00ff
+      })
+    );
+    this.scene.add(this.nameplate);
+
+    this.talkplate = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(1,1),
+      new THREE.MeshBasicMaterial({
+        color: 0x18d4d5
+      })
+    );
+    this.scene.add(this.talkplate);
 
     this.camera = new THREE.PerspectiveCamera( 70, 1, .1, 10000 );
     this.camera.target = new THREE.Vector3( 0, 0, 0 );
@@ -63,6 +94,7 @@ class Speaker extends Layer {
     speakers.forEach( s => {
       loader.load(`/assets/speakers/${s.image}`, (texture) => {
         this.speakers.set(s.id, {...s, texture});
+        this.names[s.id] = s.id;
       });
     })
   }
@@ -70,17 +102,36 @@ class Speaker extends Layer {
   selectSpeaker(id) {
     const speaker = this.speakers.get(id);
     const texture = speaker.texture;
-    this.plane.material.map = texture;
+    this.plane.material.uniforms.map.value = texture;
     const aspect = texture.image.naturalWidth / texture.image.naturalHeight;
     this.plane.scale.setScalar(4, 4*aspect, 1);
     this.plane.material.needsUpdate = true;
     texture.needsUpdate = true;
     this.speakerName.set(speaker.name);
+    this.nameplate.position.x = .0025*.9*this.speakerName.width - 1.15;
+    this.nameplate.position.y = .33;
+    this.nameplate.scale.x = .005*this.speakerName.width;
+    this.nameplate.scale.y = .5;
     this.talkTitleLines.forEach( l => l.set(''));
-    speaker.talk.forEach( (l, i) => { this.talkTitleLines[i].set(l) } );
+    let w = 0;
+    speaker.talk.forEach( (l, i) => {
+      this.talkTitleLines[i].set(l);
+      w = Math.max(w, this.talkTitleLines[i].width);
+    } );
+    this.talkplate.position.x = .0025*.9*w - 1.15;
+    this.talkplate.position.y = -.8;
+    this.talkplate.scale.x = .005*w;
+    this.talkplate.scale.y = .4 * speaker.talk.length;
   }
 
   render() {
+    const t = .001 * performance.now();
+    const f = .025 * Math.sin(t);
+    this.plane.rotation.set(f,f,f);
+    const x = .25 * Math.cos(.9*t);
+    const y = .25 * Math.sin(1.1*t);
+    this.camera.position.set(x,y,5);
+    this.camera.lookAt(this.camera.target);
     this.renderer.render(this.scene, this.camera);
   }
 
---
+++ shaders/speaker-fs.js
@@ -0,0 +1,35 @@
+export default `
+precision highp float;
+uniform sampler2D map;
+varying vec2 vUv;
+
+#define M_PI 3.1415926535897932384626433832795
+
+float luma(vec3 color) {
+  return dot(color, vec3(0.299, 0.587, 0.114));
+}
+
+vec3 blend(vec3 target, vec3 blend) {
+  return 1. - (1.-target) * (1.-blend);
+}
+
+void main() {
+  vec4 c = texture2D(map, vUv);
+  float l = luma(c.rgb);
+  vec3 color1 = vec3(1.,0.,1.);
+  vec3 color2 = vec3(0.,0.,1.);
+
+  vec2 center = vec2(.5);
+  vec2 scale = vec2(1.);
+  float angle = M_PI / 8.;
+  vec2 a = center - vec2(scale.x*cos(angle), scale.y*sin(angle));
+  vec2 b = center + vec2(scale.x*cos(angle), scale.y*sin(angle));
+
+  vec2 ba = b - a;
+  float t = dot(vUv - a, ba) / dot(ba, ba);
+  t = smoothstep(0.0, 1.0, clamp(t, 0.0, 1.0));
+  vec3 g = mix(color1, color2, t);
+
+  gl_FragColor = vec4(blend(c.rgb,g), .9 + .1* l);
+}
+`;
