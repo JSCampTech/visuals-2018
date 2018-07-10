9c3d0d4e10c3359d32fdacd42db681acc08a9401
Jaume Sanchez <the.spite@gmail.com>
2018-07-10 23:42:47 +0100

    added speakers

--- assets/speakers.js
+++ assets/speakers.js
@@ -1,6 +1,48 @@
 export default [
   {
-    id: 'michaela-lehr',
+    id: 'ashleywilliams',
+    name: 'Ashley Williams',
+    talk: [
+      'Opening Keynote'
+    ],
+    image: 'ashley.jpg'
+  },
+  {
+    id: 'mikehartington',
+    name: 'Mike Hartington',
+    talk: [
+      'Rise above the Framework'
+    ],
+    image: 'mike.jpg'
+  },
+  {
+    id: 'benediktmeurer',
+    name: 'Benedikt Meurer',
+    talk: [
+      'A Tale of Types, Classes,',
+      'and Maps'
+    ],
+    image: 'benedikt-meurer.jpg'
+  },
+  {
+    id: 'kylesimpson',
+    name: 'Kyle Simpson',
+    talk: [
+      'Keep Betting On JavaScript'
+    ],
+    image: 'kyle-simpson.jpg'
+  },
+  {
+    id: 'johannesewald',
+    name: 'Johannes Ewald',
+    talk: [
+      'The Bumpy Road',
+      'to Universal JavaScript'
+    ],
+    image: 'johannes-ewald.jpg'
+  },
+  {
+    id: 'michaelalehr',
     name: 'Michaela Lehr',
     talk: [
       'WebXR: A New Dimension For The Web',
@@ -10,9 +52,76 @@ export default [
     image: 'michaela-lehr.jpg'
   },
   {
-    id: 'sean-larkin',
+    id: 'seanlarkin',
     name: 'Sean Larkin',
-    talk: ['Building a Platform:', 'Webpack and the Future'],
+    talk: [
+      'Building a Platform:',
+      'Webpack and the Future'
+    ],
     image: 'seanlarkin.jpg'
-  }
+  },
+  {
+    id: 'kamilmysliwiec',
+    name: 'Kamil Mysliwiec',
+    //name: 'Kamil Myśliwiec',
+    talk: [
+      'Rethinking Enterprise Architectures',
+      'with Node.js'
+    ],
+    image: 'kamilmysliwiec.jpg'
+  },
+  {
+    id: 'evanschultz',
+    name: 'Evan Schultz',
+    talk: [
+      'How to Create \'Vuetiful\'',
+      'Data-Driven User Interfaces'
+    ],
+    image: 'evan-schultz.jpg'
+  },
+  {
+    id: 'hassandjirdeh',
+    name: 'Hassan Djirdeh',
+    talk: [
+      'Understanding client side routing',
+      'with Vue.js',
+    ],
+    image: 'hassan-djirdeh.jpg'
+  },
+  {
+    id: 'anujnair',
+    name: 'Anuj Nair',
+    talk: [
+      'Building Real-World Desktop Apps',
+      'with Electron',
+    ],
+    image: 'anuj-nair.jpg'
+  },
+  {
+    id: 'elisabethengel',
+    name: 'Elisabeth Engel',
+    talk: [
+      'Break Up With Your Frontend Monolith',
+
+    ],
+    image: 'elisabeth-engel.jpg'
+  },
+  {
+    id: 'lianli',
+    name: 'Lian Li',
+    talk: [
+      'Machine Learning in the browser',
+    ],
+    image: 'lian-li.jpg'
+  },
+  {
+    id: 'kennethchristiansen',
+    name: 'Kenneth Christiansen',
+    talk: [
+      'Service Workers + Web Assembly:',
+      'Experience implementing a WebP decoder'
+    ],
+    image: 'kenneth-christiansen.jpg'
+  },
+
 ]
